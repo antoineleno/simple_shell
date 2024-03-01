a5d64b45124f3988cd32509811574951c9b0bb75
antoineleno <johndoe@example.com>
2024-03-02 02:13:13 +0800

    Simple shell

--- shell.c
+++ shell.c
@@ -125,10 +125,12 @@ void execute_commands(char **command, int count, char *argv)
 					exit(126);
 				exit(1);
 			}
+			
 		}
 		else
 		{
 			waitpid(pid, &status, 0);
+			exit(2);
 		}
 	}
 	else
